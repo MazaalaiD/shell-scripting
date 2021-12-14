c66e36c15d96626acee56dcef890337f718b7be6
Raphael Hage <mazaaalai@gmail.com>
2021-12-15 00:03:37 +0100

    ls_ordered.sh

---
+++ exercise-2/ls_ordered.sh
@@ -0,0 +1,7 @@
+l pour lister les fichiers
+-ls pour avoir les fichier en mode long
+-lS pour classer les fichiers par tailles
+
+on trouve les deux premieres commandes dans le man ls mais la troisieme je l'ai trouvé sur interenet
+j'ai rencontré des problemes mais ca a été .
+
